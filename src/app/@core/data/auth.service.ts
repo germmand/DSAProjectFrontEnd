62fad5cf81cb130a0d78914db32ff0fc2f603496
germmand <german.aguilera17@gmail.com>
2018-09-13 20:07:16 -0400

    WIP: Refresh token's flow.

--- src/app/@core/data/auth.service.ts
+++ src/app/@core/data/auth.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { GetEndPointFullPath } from '../utils/api.config';
+import { IAppState } from '../store/app.reducer';
+import { select, Store } from '@ngrx/store';
+import { getRefreshToken } from '../store/auth';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
   private httpOptions;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private store: Store<IAppState>) {
     this.httpOptions = {
-      headers: new Headers({
+      headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
     };
@@ -26,5 +31,17 @@ export class AuthService {
     return this.http.post(GetEndPointFullPath('/users/'),
                           registrationForm.value,
                           this.httpOptions);
+  }
+
+  onRefreshToken(): Observable<any> {
+    return this.store.pipe(
+      select(getRefreshToken),
+      switchMap(refresh_token => {
+          return this.http.get(GetEndPointFullPath('/auth/refresh'), {
+            headers: new HttpHeaders({
+              'Authorization': 'Bearer ' + refresh_token,
+            }),
+          });
+      }));
   }
 }
--- src/app/@core/interceptors/token.interceptor.ts
+++ src/app/@core/interceptors/token.interceptor.ts
@@ -1,13 +1,90 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpInterceptor, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IAppState } from '../store/app.reducer';
+import { select, Store } from '@ngrx/store';
+import { getAccessToken, SignIn } from '../store/auth';
+import { catchError, filter, switchMap, take } from 'rxjs/operators';
+import { AuthService } from '../data/auth.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor() {
+  private refreshTokenInProgress: boolean;
+  private accessTokenSubject: BehaviorSubject<any>;
+
+  constructor(private store: Store<IAppState>,
+              private authService: AuthService) {
+    this.refreshTokenInProgress = false;
+    this.accessTokenSubject = new BehaviorSubject<any>(null);
   }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req);
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return this.store.pipe(
+      select(getAccessToken),
+      switchMap(access_token => {
+        const authReq = req.clone({
+          setHeaders: {
+            Authorization: 'Bearer ' + access_token,
+          },
+        });
+
+        return next.handle(authReq);
+      }),
+      catchError(error => {
+        if (error instanceof HttpErrorResponse) {
+          const errorResponse = <HttpErrorResponse>(error);
+
+          switch (errorResponse.status) {
+            case 401:
+              if (!this.refreshTokenInProgress) {
+                this.refreshTokenInProgress = true;
+                this.accessTokenSubject.next(null);
+
+                return this.onHandling401Unauthorized(req, next, errorResponse);
+              }
+
+              return this.accessTokenSubject.pipe(
+                filter(token => token !== null),
+                take(1),
+                switchMap(token => {
+                  const authReq = req.clone({
+                    setHeaders: { Authorization: 'Bearer ' + token },
+                  });
+
+                  return next.handle(authReq);
+                }),
+              );
+          }
+        }
+
+        return Observable.throw(error);
+      }));
+  }
+
+  onHandling401Unauthorized(req: HttpRequest<any>, next: HttpHandler, error: HttpErrorResponse) {
+    return this.authService.onRefreshToken().pipe(
+      switchMap(response => {
+        this.store.dispatch(new SignIn({
+          access_token: response.access_token,
+          refresh_token: response.refresh_token,
+        }));
+
+        this.accessTokenSubject.next(response.access_token);
+        const authReq = req.clone({
+          setHeaders: {
+            Authorization: 'Bearer ' + response.access_token,
+          },
+        });
+
+        this.refreshTokenInProgress = false;
+
+        return next.handle(authReq);
+      }),
+      catchError(exception => {
+        this.refreshTokenInProgress = false;
+
+        return Observable.throw(exception);
+      }),
+    );
   }
 }
