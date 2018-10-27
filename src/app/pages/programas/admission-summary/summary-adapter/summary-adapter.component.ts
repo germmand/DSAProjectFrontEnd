755c49d8aff8de8f5945167a83946d84e84adb82
germmand <german.aguilera17@gmail.com>
2018-10-26 23:06:13 -0400

    WIP: Admission Summary Component.

--- src/app/pages/programas/admission-summary/summary-adapter/summary-adapter.component.ts
+++ src/app/pages/programas/admission-summary/summary-adapter/summary-adapter.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
 import { ISubjectSummary } from '../../@interfaces';
 
 @Component({
@@ -6,9 +7,44 @@ import { ISubjectSummary } from '../../@interfaces';
   templateUrl: './summary-adapter.component.html',
   styleUrls: ['./summary-adapter.component.scss'],
 })
-export class SummaryAdapterComponent {
-  @Input() public subjects: ISubjectSummary[];
+export class SummaryAdapterComponent implements OnInit {
+  @Input() public subjects: ISubjectSummary[] = [];
+  public tableSource: LocalDataSource = new LocalDataSource();
+  public tableSettings: any = {
+    actions: false,
+    noDataMessage: 'No hay materias que cargar acá...',
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      name: {
+        title: 'Nombre',
+        type: 'string',
+      },
+      subject_semester: {
+        title: 'Semester',
+        type: 'number',
+      },
+      credits: {
+        title: 'Créditos',
+        type: 'number',
+      },
+      hours_per_week: {
+        title: 'Horas/Semana',
+        type: 'number',
+      },
+      weeks: {
+        title: 'Semanas',
+        type: 'string',
+      },
+    },
+  };
 
   constructor() {
+  }
+
+  ngOnInit() {
+    this.tableSource.load(this.subjects.map(s => s.subject));
   }
 }
