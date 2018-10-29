4d784666fc594f22750edfbcd932c10fc9cdc69f
germmand <german.aguilera17@gmail.com>
2018-10-29 16:13:54 -0400

    WIP: Adding new program.s

--- src/app/pages/admin/handle-courses/handle-programs/handle-programs.component.ts
+++ src/app/pages/admin/handle-courses/handle-programs/handle-programs.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AreasService } from '../../../../@core/data/areas.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IArea } from '../@interfaces';
 
 @Component({
   selector: 'ngx-handle-programs',
   templateUrl: './handle-programs.component.html',
   styleUrls: ['./handle-programs.component.scss'],
 })
-export class HandleProgramsComponent {
-  constructor() {
+export class HandleProgramsComponent implements OnInit {
+  public newCoursesForm: FormGroup;
+  public areas: IArea[];
+
+  constructor(private areasService: AreasService) {
+  }
+
+  ngOnInit() {
+    this.newCoursesForm = new FormGroup({
+      area_id: new FormControl('', [Validators.required]),
+    });
+
+    this.areasService
+      .onGetAllAreas()
+      .subscribe(response => {
+        this.areas = <IArea[]>response['areas'];
+      });
+  }
+
+  onAddingProgram() {
+  }
+
+  onAddingNewPrograms() {
+  }
+
+  get coursesForm(): any {
+    return this.newCoursesForm.controls;
   }
 }
